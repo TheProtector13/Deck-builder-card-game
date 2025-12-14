096a2d07456c82831c1c2deceb473fba392b7a92
László Szoboszlai <lacuka111@gmail.com>
2025-12-14 21:25:30 +0100

    Monogame pipeline auto-generate texture and other content on build DISABLED!

--- CardGame/CardGame/obj/Debug/net8.0-windows/CardGame.AssemblyInfo.cs
+++ CardGame/CardGame/obj/Debug/net8.0-windows/CardGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ade1a1599d559c60035b517636fa75cd2f12fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
